3260a591f4bd22430482e4e8b27547c8c7ea2c88
段佳涛 <djt317@139.com>
2021-06-24 19:38:11 +0800

    构建服务基础框架

--- x-api/src/main/resources/start.sh
+++ x-api/src/main/resources/start.sh
@@ -6,6 +6,6 @@ JAR=${artifactId}-${version}.${packaging}
 LOCATION=$DEPLOY_DIR/lib/$JAR
 
 LOG=-Dlogging.config=$CONF_DIR/log4j2.xml
-ARGS=
+ARGS=--spring.config.location=$CONF_DIR/application.properties
 
 java -server -Xms512m -Xmx1g -Xmn256m -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=70 -XX:+CMSParallelRemarkEnabled -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:+DisableExplicitGC -verbose:gc -Xloggc:$DEPLOY_DIR/gc.log -XX:+PrintGCDetails -XX:-OmitStackTraceInFastThrow $LOG -jar $LOCATION $ARGS
